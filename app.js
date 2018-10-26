c96716a14b4f3aab41dce5f88eb93e3b5f8ad63d
Alex Roman <ialex.roman@icloud.com>
2018-10-26 12:08:41 -0400

    added some more code to the project.. still learning son

--- app.js
+++ app.js
@@ -9,19 +9,9 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
@@ -40,36 +30,93 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //Next Player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //turnerary Operator look this term up
-        roundScore = 0;
-        //******************** */
-        //same thing as writing
-        //if(activePlayer === 0) {
-        //     activePlayer = 1;
-        // }   else {
-        //     activePlayer = 0;
-        // }
-        //******************///
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
+    //     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //turnerary Operator look this term up
+    //     roundScore = 0;
+    //     //******************** */
+    //     //same thing as writing
+    //     //if(activePlayer === 0) {
+    //     //     activePlayer = 1;
+    //     // }   else {
+    //     //     activePlayer = 0;
+    //     // }
+    //     //******************///
+    //     document.getElementById('current-0').textContent = '0';
+    //     document.getElementById('current-1').textContent = '0';
 
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+    //     document.querySelector('.player-0-panel').classList.toggle('active');
+    //     document.querySelector('.player-1-panel').classList.toggle('active');
 
-        //*****************this is how you add and remove class properties*********** */
-       // document.querySelector('.player-0-panel').classList.remove('active');
-       // document.querySelector('.player-1-panel').classList.add('active');
+    //     //*****************this is how you add and remove class properties*********** */
+    //    // document.querySelector('.player-0-panel').classList.remove('active');
+    //    // document.querySelector('.player-1-panel').classList.add('active');
 
-        document.querySelector('.dice').style.display = 'none'; // Lesson finishes on Lecture video 51
+    //     document.querySelector('.dice').style.display = 'none'; // Lesson finishes on Lecture video 51
+
+        //Next Player
+        nextPlayer();
     }
     
     // end of btn function..
     } )
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current score to GLOBAL score
+    scores[activePlayer] += roundScore;    // same as scores[activePlayer] + scores[activePlayer] = roundScores;
+
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // Check if player won the game
+    if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('#name-' + activePlayer).style.fontWeight = '800';
+
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+
+    }   else {
+        //Next Player
+        nextPlayer();
+    }
+    
+});
+
+
+function nextPlayer() {
+     //Next Player
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //turnerary Operator look this term up
+     roundScore = 0;
+
+     document.getElementById('current-0').textContent = '0';
+     document.getElementById('current-1').textContent = '0';
+
+     document.querySelector('.player-0-panel').classList.toggle('active');
+     document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+     document.querySelector('.dice').style.display = 'none'; 
+}
+
+document.querySelector('btn-new').addEventListener('click', init);
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.getElementById('name-0').textContent = 'player 1';
+    document.getElementById('name-1').textContent = 'player 2';
 
 
 
 
+}
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activeplayer).innerHTML = '<em>' + dice + '</em>';
