2c630d7eab30b8c45abad0274d8df507881fe563
Alex Roman <ialex.roman@icloud.com>
2018-10-28 13:53:47 -0400

    added some Object lessons on dieh righ chea

---
+++ objects/objects.js
@@ -0,0 +1,13 @@
+var john = {
+    name: 'john',
+    yearOfBirth: 19998,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+
