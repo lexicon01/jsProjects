eb644b959c2700fc64bd2427c4b9da523669bd29
Alex Roman <ialex.roman@icloud.com>
2018-10-28 14:24:35 -0400

    added some notes to objects in js

--- objects/objects.js
+++ objects/objects.js
@@ -4,10 +4,17 @@ var john = {
     job: 'teacher'
 };
 
+
+// THIS IS CALLED A FUNCTION CONSTRUCTOR
 var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
 }
 
+
+// THIS IS HOW YOU FILL IN A FUNCTION CONSTRUCTOR
+// BY ADDING A new ____<= [name of obj constructor]
 var john = new Person('john', 1998, 'teacher');
+
+console.log(john.yearOfBirth);
