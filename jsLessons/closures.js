b384503090b21fcd7917e2b9dfa185f975b91296
Alex Roman <ialex.roman@icloud.com>
2018-11-01 12:03:12 -0400

    update

--- jsLessons/closures.js
+++ jsLessons/closures.js
@@ -27,7 +27,60 @@ var retirementUS = retirement(66);
  retirementIceland(1990);
  
  
+ /*  
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function(name) {
+            console.log('hello ' + name + ', what do you do?');
+        }
+    }
+}
+ */
+
+ /*
+            YOUR ATTEMPT ...LOL 
+
+ function askjob(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explain what UX design is?');
+        } 
+    } else askjob(job) 
+        if (job === 'teacher') {
+            return function(name) {
+                console.log(name + 'what subject do you teach, ' + name + '?');
+            }
+        } else return function(name) {
+            console.log('hello' + name + ', what do you do?');
+        }
+    }
+ }
  
- 
+  var workQ = askjob('designer');
+
+  */
+
+  workQ('designer');
+
+  function interviewQuestion(job) {
+      return function(name) {
+          if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+          } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+          }  else {
+            console.log('hello ' + name + ', what do you do?');
+          }
+      }
+  }
  
  //retirement(66)(1998);
