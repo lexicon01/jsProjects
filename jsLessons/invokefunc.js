5380ca9a25ab44a0140fca7173b41a5fa700571b
Alex Roman <ialex.roman@icloud.com>
2018-10-31 21:10:54 -0400

    .

--- jsLessons/invokefunc.js
+++ jsLessons/invokefunc.js
@@ -5,14 +5,21 @@
      // ** I-mediatly I-voked F-unction E-xpresion
      // Wraping it inside parainthisis declares
      // is an Expression not a Statement like normal way to make functions
-
+/*
 function game() {
     var score = Math.random() * 10;
     console.log(score >= 5);
 }
-game();
+game(); */
 
 (function () {
     var score = Math.random() * 10;
     console.log(score >= 5);
+})();
+
+console.log(score);  // cant read score outside of the paraintheis
+
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
 })();
