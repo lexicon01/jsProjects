8b49fe858386d143087a08d1048b3ac476056e0f
Alex Roman <ialex.roman@icloud.com>
2018-11-04 09:36:38 -0500

    added folder and challenge rules

---
+++ challenges/challenge7.js
@@ -0,0 +1,36 @@
+//****   CODING CHALLENGE ** */
+
+
+/*
+------Let's build a fun quiz game in the console! ====
+
+1. Build a function constructor called Question to describe a question.  A question should include:
+A) question itself
+B) the answers from which the player can choose the coreect one (choose an adequate data structure here, array, object, etc.)
+C) correct answer (I would use a number for this)
+
+2. Create a couple of question using the constructor
+
+3.  Store them all inside an array
+
+4.  Select on random Question and log it on the console,  together with the possible andswers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5.  Use the 'prompt' function to ask the user for the correct answer.  The user should input the number of the correct answer such as you displayed it on task 4.
+
+6.  Check if the ansWer is correct and print to the console wherther the answer is correct at nor (hint: write another method for this).
+
+7.  Suppose this code would be a plugin for other programmers to use in their code.  So make sure that all your code is private and doesn't interfere with other programmers code (Hint: we learned a special technique to do exactly )
+that).
+
+====================== EXPERT LEVEL ===================
+
+8.  After you display the result, display the next random question, so that the game never ends( Hint: write a function for this and call it right after displaying the result)
+
+9. becareful: after Task 8, the game literally never ends  So include the option to quit the game if the users writes 'exit' instead of the answer.  In this case, DONT call the function from task 8.
+
+10.  Track the user's score to make the game more fun! so each time an anser is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you dont have to, just do this with the tools you feel more confortable at this point).
+
+11. Display the score in the console.  Use yet another method for this.
+
+
+*/
