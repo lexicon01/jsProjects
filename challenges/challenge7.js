43d13a39fc6097d54844b59a3eeec9681cc45df7
Alex Roman <ialex.roman@icloud.com>
2018-11-04 19:34:25 -0500

    finished challenge code along..

--- challenges/challenge7.js
+++ challenges/challenge7.js
@@ -35,6 +35,8 @@ that).
 
 */
 
+/*
+
 (function() {
 
     function Question(question, answers, correct) {
@@ -64,7 +66,7 @@ that).
         ['yes', 'no'], 0); // the NEW keyword add this as a variable to the this.Question
     
     var q2 = new Question('what is the name of this course\'s teacher?', 
-        ['john', 'Micheal', 'Jonas'], 1);
+        ['john', 'Micheal', 'Jonas'], 2);
     
     var q3 = new Question('what does best describe coding?',
         ['boring', 'hard', 'fun', 'tedius'], 2);
@@ -78,6 +80,95 @@ that).
     var answer = parseInt(prompt('Please select the correct answer.'));
     
     questions[n].checkAnswer(answer);
+    
+
+}) ();
+
+*/
+
+
+(function() {
+
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+    
+    Question.prototype.displayQuestion =  function() {
+        console.log(this.question);
+    
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+    
+    Question.prototype.checkAnswer = 
+        function(ans, callback) {
+        var sc;
+        if (ans === this.correct) {
+            console.log('correct answer!');
+            sc = callback(true);
+        } else {
+            console.log('Wrong answer try again');
+            sc = callback(false);
+        }
+
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = 
+    function(score) {
+        console.log('Your current score is : ' + score);
+        console.log('--------------------------------')
+    }
+
+
+    var q1 = new Question('Is javascript the coolest programming language in the world?', 
+    ['yes', 'no'], 0); // the NEW keyword add this as a variable to the this.Question
+
+    var q2 = new Question('what is the name of this course\'s teacher?', 
+    ['john', 'Micheal', 'Jonas'], 2);
+
+    var q3 = new Question('what does best describe coding?',
+    ['boring', 'hard', 'fun', 'tedius'], 2);
+
+
+    var questions = [q1, q2, q3];
+
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            }
+            return sc;
+        }   
+    }
+    
+    var keepScore = score();
+
+    function nextQuestion() {
+        
+        var n = Math.floor(Math.random() * questions.length);
+        
+        questions[n].displayQuestion();
+        
+        var answer = prompt('Please select the correct answer.');
+        
+        
+
+        if (answer !== 'exit') {
+
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+
+            nextQuestion();
+        }
+
+       
+    }
+    
+    nextQuestion();
     
 
 }) ();
