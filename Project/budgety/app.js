1a48e9b22c53fde778134d7ee6591d7fd9312410
Alex Roman <ialex.roman@icloud.com>
2018-11-05 21:19:46 -0500

    found the bug.. I.. lol

--- Project/budgety/app.js
+++ Project/budgety/app.js
@@ -18,11 +18,12 @@ var UIController = (function() {
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
-        inputValue: '.add__value'
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
     };
 
     return {
-        getinput: function() {
+        getInput: function() {
             return {
 
                  type:  document.querySelector(DOMstrings.inputType).value,  // will be either inc or exp
@@ -30,8 +31,12 @@ var UIController = (function() {
                  value:  document.querySelector(DOMstrings.inputValue).value
             };
 
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
-    }
+    };
 
 })();
 
@@ -39,10 +44,12 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. get the filed input data
-        var input = UICtrl.getinput();
+        var input = UICtrl.getInput();
         console.log(input);
 
         // 2. add the item to the budget controller
@@ -55,7 +62,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
