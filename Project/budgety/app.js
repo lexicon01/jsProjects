5eeed7a10581a6bd3e36b66718b44d85c3668a59
Alex Roman <ialex.roman@icloud.com>
2018-11-08 09:14:50 -0500

    finished lecture 85

--- Project/budgety/app.js
+++ Project/budgety/app.js
@@ -129,6 +129,21 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields =  document.querySelectorAll(DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -163,6 +178,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. add the item to the UI   
         UICtrl.addListItem(newItem, input.type);
+
+        //4. clear the fields
+        UICtrl.clearFields();
         // 4. calculate the budget
 
         // 5. display the budget on the UI
