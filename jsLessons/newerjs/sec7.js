69e8f45cad9f72df5956a4412a42216032f13b88
Alex Roman <ialex.roman@icloud.com>
2018-11-26 17:36:03 -0500

    added a new lesson

--- jsLessons/newerjs/sec7.js
+++ jsLessons/newerjs/sec7.js
@@ -1,10 +1,67 @@
-var person5 = function(name, yearOfBirth, job) {
+// ES6 how to connect function constructors Classes and subclasses
+// so basically you coded a function constructor and added a method es5 
+// is longer than es6
+
+var Person5 = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
 }
 
-person5.prototype.calculateAge = function() {
-    var age = new Date().getFullYear - this.yearOfBirth;
+Person5.prototype.calculateAge = function() {
+    var age = new Date().getFullYear() - this.yearOfBirth;
     console.log(age);
 }
+
+var Athlete5 = function(name, yearOfBirth, job, olymicGames, medals) {
+        Person5.call(this, name, yearOfBirth, job);
+        this.olymicGames = olymicGames;
+        this.medals = medals;
+}
+
+Athlete5.prototype = Object.create(Person5.prototype);
+
+Athlete5.prototype.wonMedal = function() {
+    this.medals++;
+    console.log(this.medals);
+}
+
+var johnAthlete5 = new Athlete5('john', 1990, 'swimmer', 3, 10);
+
+johnAthlete5.calculateAge();
+johnAthlete5.wonMedal();
+
+
+// ES6
+
+class Person6 {
+    constructor(name, yearOfBirth, job) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.job = job;
+    }
+
+    calculateAge() {
+        var age = new Date().getFullYear() - this.yearOfBirth;
+        console.log(age);
+    }
+
+}
+
+class Athlete6 extends Person6 {
+    constructor(name, yearOfBirth, job, olymicGames, medals) {
+        super(name, yearOfBirth, job);
+        this.olymicGames = olymicGames;
+        this.medals = medals;
+    }
+
+    wonMedal() {
+        this.medals++;
+        console.log(this.medals);
+    }
+}
+
+const johnAthlete6 = new Athlete6('john', 1990, 'simmer', 3, 10);
+
+johnAthlete6.wonMedal();
+johnAthlete6.calculateAge();
